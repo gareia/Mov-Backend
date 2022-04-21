b705b0b156e2de2d12c57812b8f004f7ed57bee9
Grecia <greciaguerreroa@gmail.com>
2022-04-21 11:26:03 -0500

    upload base project

---
+++ src/main/java/com/acme/erentcar/ERentCarApplication.java
@@ -0,0 +1,13 @@
+package com.acme.erentcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERentCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ERentCarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/erentcar/ERentCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.erentcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERentCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
